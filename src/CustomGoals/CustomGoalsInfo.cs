e322ecdef48e750a7126fc3a299bb52b96c64147
bulgos <dchristev@gmail.com>
2022-01-13 19:23:48 +0100

    create projects for goals

---
+++ src/CustomGoals/CustomGoalsInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace CustomGoals
+{
+    public class CustomGoalsInfo : GH_AssemblyInfo
+    {
+        public override string Name => "CustomGoals";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("4D67E0BC-9E1E-4C30-948C-E76DB2724213");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
---
+++ src/K2Goals/K2GoalsInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace K2Goals
+{
+    public class K2GoalsInfo : GH_AssemblyInfo
+    {
+        public override string Name => "K2Goals";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("7E456C88-CECB-45AB-B554-9960A43D1326");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
